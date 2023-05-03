90d45c3e8e9e15da5625bce92797552a208bbe58
NRLacerda <nrochalacerda@gmail.com>
2023-05-03 17:25:30 -0300

    att styles

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -7,20 +7,33 @@ import { MatDrawerMode } from '@angular/material/sidenav';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  showFiller:boolean;
   tipo : MatDrawerMode;
   unset:boolean;
   showNavSid:boolean;
+  SideNav: HTMLElement | null = document.getElementById('nav');
   constructor(){
+    this.showFiller= false;
     this.tipo='push'
     this.unset=false
     this.showNavSid=false
   }
 butSideMenu(){
   if(this.showNavSid){
-    this.showNavSid=false
+  //  this.showNavSid=false;
+    this.ShowSideNav(true);
   }else{
-    this.showNavSid=true
-   //drawer.toggle()
+   // this.showNavSid=true
+    this.ShowSideNav(false);
+
+  }
+}
+ShowSideNav(open:boolean){
+  
+  if(open){
+    this.SideNav?.classList.add('SideNav');
+  }else{
+    this.SideNav?.classList.remove('SideNav')
   }
 }
 }
