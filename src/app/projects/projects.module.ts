d4326aff508612da461c7c2a6d9db6644ad79d51
NRLacerda <nrochalacerda@gmail.com>
2023-05-05 10:18:13 -0300

    reposy

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,15 +3,27 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects.routing.module';
 import { ProjectsComponent } from './projects.component';
 import { HomeComponent } from '../home/home.component';
+import { RepositoryComponent } from './repository/repository.component';
+import {MatCardModule} from '@angular/material/card'; 
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
     ProjectsComponent,
-    HomeComponent
+    HomeComponent,
+    RepositoryComponent
   ],
   imports: [
     CommonModule,
-    ProjectsRoutingModule
+    ProjectsRoutingModule,
+    MatCardModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatButtonModule
   ]
 })
 export class ProjectsModule { }
